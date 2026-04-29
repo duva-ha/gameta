b6c243dcefeb12a078abec7d14cddd745c15b538
duva-ha <dvhai.vi@gmail.com>
2026-04-29 16:56:30 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gameta/', // Thay 'gameta' bằng tên repository của bạn
+  // Dòng này cực kỳ quan trọng để sửa lỗi 404 trên Vercel/GitHub Pages
+  base: './', 
 })
